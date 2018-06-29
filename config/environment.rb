417ca3fe9637b9e642950d7342b84710d3eb6449
Maxim Zherebtsov <getmax4444@gmail.com>
2018-06-29 21:43:38 +0000

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.stmp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT']
-    :address        => ENV['MAILGUN_SMTP_SERVER']
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN']
-    :password       => ENV['MAILGUN_SMTP_PASSWORD']
-    :domain         => 'powerful-ravine-72631.herokuapp.com',
-    :authentication => :plain,
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'powerful-ravine-72631.herokuapp.com',
+  :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
