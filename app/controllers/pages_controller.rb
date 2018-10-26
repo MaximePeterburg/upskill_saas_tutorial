6b3e04c5ce141bafc368a3c3aafcc6001f82486d
Maxim Zherebtsov <getmax4444@gmail.com>
2018-10-26 12:12:32 +0000

    Added plan vars to home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+    # GET request for  / which is nothing
     def home
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
     end
     def about
     end
