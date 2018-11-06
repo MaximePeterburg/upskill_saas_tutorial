b045ecc8ad534ea01a27a1243a3f5857194d0d6e
Maxim Zherebtsov <getmax4444@gmail.com>
2018-11-06 13:38:25 +0000

    added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,11 @@
 class ProfilesController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :only_current_user
   # GET to /users/:user_id/profile/new
-  
   def new
     #Render blank profile details form
     @profile = Profile.new
   end
-  
   
   # POST to /users/:user_id/profile
   def create
@@ -20,6 +19,12 @@ class ProfilesController < ApplicationController
     else
       render action: :new
     end
+  end
+  
+  # GET to /users/:user_id/profile/edit
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
   end
   
   #PATCH to /users/:user_id/profile
@@ -38,14 +43,13 @@ class ProfilesController < ApplicationController
     end
   end
   
-  # GET to /users/:user_id/profile/edit
-  def edit
-    @user = User.find( params[:user_id] )
-    @profile = @user.profile
-  end
-  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)  
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
 end
